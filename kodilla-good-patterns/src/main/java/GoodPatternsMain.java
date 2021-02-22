1ee8399c7ed5f2c3df04194d16e165cef56d35c5
Mateusz Dziadowicz <dziadulus@hotmail.com>
2021-02-22 17:57:45 +0100

    Added goodPatterns MovieStore class

---
+++ kodilla-good-patterns/src/main/java/GoodPatternsMain.java
@@ -0,0 +1,11 @@
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+public class GoodPatternsMain {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.moviesToString());
+    }
+}
