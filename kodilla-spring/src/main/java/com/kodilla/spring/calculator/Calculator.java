e495a1e1016cfbb650343a0a742eda4773381517
Mateusz Dziadowicz <dziadulus@hotmail.com>
2021-03-01 16:39:28 +0100

    14.2 calculator improved

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -10,25 +10,37 @@ public final class Calculator {
     @Autowired
     private Display display;
 
-    public void add(double a, double b) {
+    public double add(double a, double b) {
         double result = a + b;
         display.displayValue(result);
+
+        return result;
     }
 
-    public void sub(double a, double b) {
+    public double sub(double a, double b) {
         double result = a - b;
         display.displayValue(result);
+
+        return result;
     }
 
-    public void mul(double a, double b) {
+    public double mul(double a, double b) {
         double result = a * b;
         display.displayValue(result);
+
+        return result;
     }
 
-    public void div(double a, double b) {
+    public double div(double a, double b) {
         if (b!=0) {
             double result = a / b;
             display.displayValue(result);
+
+            return result;
+        } else {
+            System.out.println("Divining by 0 is impossible.");
+
+            return 0;
         }
     }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -15,7 +15,7 @@ public class CalculatorTestSuite {
         //When
         calculator.add(2, 3);
         calculator.sub(3, 2);
-        calculator.div(4,2);
+        calculator.div(4,0);
         calculator.mul(5, 10);
         //Then
         //do nothing
